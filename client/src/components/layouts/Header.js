00b209066bf32fccd61b0a8a469a84752d7016d0
Oleh <zhyriki@ukr.net>
2019-01-12 20:55:43 +0200

    optimization Header component

--- client/src/components/layouts/Header.js
+++ client/src/components/layouts/Header.js
@@ -1,25 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
+import { withStyles } from '@material-ui/core/styles'
 
-export default props =>
+const styles = {
+  navLink: {
+    color: '#ffffff',
+    textDecoration: 'none'
+  }
+}
+
+const Header = ({ classes }) =>
 <div>
   <AppBar position="static">
     <Toolbar>
       <IconButton color="inherit" aria-label="Menu">
         <MenuIcon />
       </IconButton>
-      <NavLink to='/' exact style={{color: '#ffffff', textDecoration: 'none'}}>
+      <NavLink to='/' exact className={ classes.navLink }>
         <Button color="inherit" >Zhurik</Button>
       </NavLink>
-      <Typography style={{flexGrow: 1}}></Typography>
-      <NavLink to='/login' style={{color: '#ffffff', textDecoration: 'none'}}>
+      <Typography style={{flexGrow: 1}}> </Typography>
+      <NavLink to='/login' className={ classes.navLink }>
         <Button color="inherit" >Login</Button>
       </NavLink>
-      <NavLink to='/add' style={{color: '#ffffff', textDecoration: 'none'}}>
+      <NavLink to='/add' className={ classes.navLink }>
         <Button color="inherit" >add own categoris</Button>
       </NavLink>
     </Toolbar>
   </AppBar>
 </div>
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Header)

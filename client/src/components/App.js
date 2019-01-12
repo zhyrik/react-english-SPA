0345436bda8b22eabad1728346476ce2f4b448e0
Oleh <zhyriki@ukr.net>
2019-01-12 21:34:13 +0200

    optimization App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 import Header from './layouts/Header'
 import Footer from './layouts/Footer'
@@ -7,23 +7,21 @@ import Home from './layouts/Home'
 import Learn from './layouts/learn/'
 import AddCategory from './layouts/addCategory'
 
-// import axios from 'axios'
-
-export default class extends Component {
-  // componentDidMount () {
-  //   axios.get('https://react-english.firebaseio.com/quiz.json')
-  //     .then(response => console.log(response))
-  // }
-  render () {
-    return (
-      <div>
-        <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/learn" component={Learn} />
-        <Route path="/login" component={Auth} />
-        <Route path="/add" component={AddCategory} />
-        <Footer />
-      </div>
-    )
-  }
+const styles = {
+  margin: '0 auto',
+  minHeight: '100vh',
+  maxWidth: '1200px',
+  display: 'grid',
+  gridTemplateRows: '64px 1fr 80px'
 }
+
+export default () => (
+  <div style={ styles }>
+    <Header />
+    <Route path="/" exact component={Home} />
+    <Route path="/learn" component={Learn} />
+    <Route path="/login" component={Auth} />
+    <Route path="/add" component={AddCategory} />
+    <Footer />
+  </div>
+)

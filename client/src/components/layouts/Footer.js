50cb104175f0f03e43bd07966019c47aeb7ce1de
Oleh <zhyriki@ukr.net>
2019-01-12 21:03:15 +0200

    optimization Footer component

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Paper, Typography, Fab } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 
-const style = {
+const styles = {
   text: {
     textAlign: 'center'
   },
@@ -13,38 +14,47 @@ const style = {
     margin: 10
   }
 }
-export default props => {
-const links = [
-  {
-    link: 'https://www.facebook.com/zhyriki',
-    icon: 'fb'
-  },
-  {
-    link: 'https://www.facebook.com/zhyriki',
-    icon: 'fb'
-  },
-  {
-    link: 'https://www.facebook.com/zhyriki',
-    icon: 'fb'
-  },
-  {
-    link: 'https://www.facebook.com/zhyriki',
-    icon: 'fb'
-  },
-]
 
-return <div>
-  <Paper elevation={1} >
-    <div style={style.wrap}>
-      {links.map((link, index) => {
-        return <Fab variant="contained" href={link.link} size= "small" style={style.social} key={index} >
-        {link.icon}
-      </Fab>
-      })}
-    </div>
-    <Typography component="p" style={style.text} color="primary">
-      Copyright © 2018 Oleh Zhurovskyi. All rights reserved.
-    </Typography>
-  </Paper>
-</div>
+const Footer = ({ classes }) => {
+  const links = [
+    {
+      link: 'https://www.facebook.com/zhyriki',
+      icon: 'fb'
+    },
+    {
+      link: 'https://www.facebook.com/zhyriki',
+      icon: 'fb'
+    },
+    {
+      link: 'https://www.facebook.com/zhyriki',
+      icon: 'fb'
+    },
+    {
+      link: 'https://www.facebook.com/zhyriki',
+      icon: 'fb'
+    },
+  ]
+
+  return <div>
+    <Paper elevation={1} >
+      <div className={ classes.wrap }>
+        {links.map((link, index) => {
+          return <Fab
+            variant="contained"
+            href={link.link}
+            size= "small"
+            key={index}
+            className={ classes.social }
+          >
+          {link.icon}
+        </Fab>
+        })}
+      </div>
+      <Typography component="p" className={ classes.text } color="primary">
+        Copyright © 2018 Oleh Zhurovskyi. All rights reserved.
+      </Typography>
+    </Paper>
+  </div>
 }
+
+export default withStyles(styles)(Footer)
